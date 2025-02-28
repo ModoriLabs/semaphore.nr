688ffe0aa8ab1029b794710dd116310f4692097c
qpzm <lhm1442@gmail.com>
2025-02-28 17:49:51 +0900

    feat: add js script to create proof

---
+++ packages/js/generate-proof.ts
@@ -0,0 +1,34 @@
+import { UltraHonkBackend } from "@aztec/bb.js";
+import fs from "fs";
+import circuit from "../semaphore/target/semaphore.json";
+// @ts-ignore
+import { Noir } from "@noir-lang/noir_js";
+
+
+(async () => {
+  try {
+    const noir = new Noir(circuit as any);
+    const honk = new UltraHonkBackend(circuit.bytecode, { threads: 1 });
+
+    const inputs = {
+      root: "0x1c709df0fc4393283665d98bd1f73a461a6a74f57c45ece9c192a88a389f8fd9",
+      indexes: "4",
+      message: "1",
+      paths: ["0", "0", "0x1d04d957ef6364c51a27258dd9ca5622e2113cba6858e4634fdb2ff12743c909", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+      scope: "1",
+      secret: "5"
+    }
+    const { witness } = await noir.execute(inputs);
+    const { proof, publicInputs } = await honk.generateProof(witness, { keccak: true });
+
+    const cleanProof = proof.slice(4); // remove first 4 bytes (buffer size)
+
+    // save proof to file
+    fs.writeFileSync("../semaphore/target/proof-clean", cleanProof);
+
+    // not really needed as we harcode the public input in the contract test
+    fs.writeFileSync("../semaphore/target/public-inputs", JSON.stringify(publicInputs));
+  } catch (error) {
+    console.error(error);
+  }
+})();
