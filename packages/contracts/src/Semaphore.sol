98ab02c0b4026a3e4db2851a2aba3e84bdd20614
qpzm <lhm1442@gmail.com>
2025-02-28 17:47:43 +0900

    feat: add HonkVerifier.sol

---
+++ packages/contracts/src/Semaphore.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity 0.8.28;
+
+import {SemaphoreGroups} from "./base/SemaphoreGroups.sol";
+
+contract Semaphore is SemaphoreGroups {
+    function addMember(
+        uint256 identityCommitment
+    ) public {}
+}
