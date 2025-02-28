3018db023bbb1c8c743b5dc72b77d755bf38a60d
qpzm <lhm1442@gmail.com>
2025-02-28 17:49:51 +0900

    feat: add js script to create proof

---
+++ packages/semaphore/script/build.sh
@@ -0,0 +1,15 @@
+set -e
+
+echo "Compiling circuit..."
+if ! nargo compile; then
+    echo "Compilation failed. Exiting..."
+    exit 1
+fi
+
+echo "Generating vkey..."
+bb write_vk_ultra_keccak_honk -b ./target/semaphore.json -o ./target/vk -c ~/.bb-crs
+
+echo "Generating solidity verifier..."
+bb contract_ultra_honk -k ./target/vk -c ~/.bb-crs -o ./target/Verifier.sol
+
+echo "Done"
