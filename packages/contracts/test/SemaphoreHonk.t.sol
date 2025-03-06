f3fb9908c278b7934a05c6a345084c02917f6432
qpzm <lhm1442@gmail.com>
2025-03-06 18:16:30 +0900

    test: revert when different message with the same proof

--- packages/contracts/test/SemaphoreHonk.t.sol
+++ packages/contracts/test/SemaphoreHonk.t.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.28;
 
 import {Test, console} from "forge-std/Test.sol";
-import {HonkVerifier} from "../src/base/HonkVerifier.sol";
+import {HonkVerifier, BaseHonkVerifier} from "../src/base/HonkVerifier.sol";
 import {IHonkVerifier} from "../src/interfaces/IHonkVerifier.sol";
 import {SemaphoreHonk} from "../src/SemaphoreHonk.sol";
 import {ISemaphoreHonk} from "../src/interfaces/ISemaphoreHonk.sol";
@@ -18,6 +18,34 @@ contract SemaphoreHonkTest is Test {
 
     function test_CreateGroup() public {
         semaphore.createGroup();
+    }
+
+    function test_RevertWhen_ValidateProof_WithDifferentMessage() public {
+        uint256 groupId = semaphore.createGroup();
+        uint256[] memory identityCommitments = new uint256[](3);
+        identityCommitments[0] = 5407869850562333726769604095330004527418297248703115046359956082084347839061;
+        identityCommitments[1] = 18699903263915756199535533399390350858126023699350081471896734858638858200219;
+        identityCommitments[2] = 4446973358529698253939037684201229393105675634248270727935122282482202195132;
+        for (uint256 i = 0; i < identityCommitments.length; i++) {
+            semaphore.addMember(groupId, identityCommitments[i]);
+        }
+
+        bytes memory proof = vm.readFileBinary(
+            "../semaphore/target/proof-clean2"
+        );
+
+        uint256 actualMessage = 2;
+        ISemaphoreHonk.SemaphoreHonkProof memory honkProof = ISemaphoreHonk.SemaphoreHonkProof({
+            merkleTreeDepth: 32,
+            merkleTreeRoot: 0x265aad7d885c5a4d4bece302f04ac298982800c85490983594290ef693fcaf55,
+            nullifier:      0x15fc910d3c6dc6bf2362c2c1c15a8bebef733c5f6d4e1b2143e59cf9daaee6e9,
+            proof: proof,
+            message: actualMessage + 100,
+            scope: 0
+        });
+
+        vm.expectRevert(abi.encodeWithSelector(BaseHonkVerifier.SumcheckFailed.selector));
+        semaphore.validateProof(groupId, honkProof);
     }
 
     function test_ValidateProof() public {
